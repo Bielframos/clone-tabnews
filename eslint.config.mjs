c5149a34adec1a664a35982e62d9f4ad8de213c0
Gabriel França <gabrielfrancaramos99@gmail.com>
2026-06-28 10:46:36 -0300

    feat: refina configurações base do eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import json from "@eslint/json";
-import markdown from "@eslint/markdown";
 import css from "@eslint/css";
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/commonmark", extends: ["markdown/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  { files: ["tests/**/*.js", "**/*.test.js"], languageOptions: { globals: { ...globals.jest, ...globals.node } } },
+  {
+    files: ["infra/**/*.js", "jest.config.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  { ...pluginReact.configs.flat.recommended, settings: { react: { version: "18.2" } } },
+  pluginReact.configs.flat["jsx-runtime"],
 ]);
